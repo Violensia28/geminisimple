335abe431c914c0ba56cc57c79295a4363a586d7
Violensia28 <107469569+Violensia28@users.noreply.github.com>
2025-10-06 13:29:33 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@ const char* ssid = "MOTsmart_Welder_TEST";
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("\n\nMemulai Tes WiFi Minimalis...");
+  delay(1000); // Beri jeda 1 detik agar Serial Monitor siap
+  Serial.println("\n\n--- Memulai Tes WiFi Minimalis ---");
 
   WiFi.softAP(ssid);
 
-  Serial.println("Access Point Seharusnya Aktif Sekarang.");
+  Serial.println("--- Access Point Seharusnya Aktif Sekarang ---");
   Serial.print("Nama WiFi (SSID): ");
   Serial.println(ssid);
   Serial.print("Alamat IP: ");
